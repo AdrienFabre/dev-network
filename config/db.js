18a753feec9a72997b9d1220702c414bd49fdd35
Adrien Fabre <adrien.fabre.1@gmail.com>
2019-05-15 11:51:10 +0100

    Get All Profiles & Profile By User ID

--- config/db.js
+++ config/db.js
@@ -6,7 +6,8 @@ const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
-      useCreateInder: true
+      useCreateInder: true,
+      useFindAndModify: false
     });
 
     console.log("MongoDB Connected...");
